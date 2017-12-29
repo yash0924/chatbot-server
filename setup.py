1424b41141b4e640aca4ce640075d87c0c015820
nahomwosenu <nahometete@gmail.com>
2017-12-29 05:00:59 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import nltk
+import os
+import traceback
+
+# Downloading necessary NLTK datasets
+nltk.download("stopwords")
+nltk.download("wordnet")
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
+
+# creating directory for storing chat logs
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
+try:
+    print("Training models..")
+    from app.core.intentClassifier import IntentClassifier
+    IntentClassifier().train()
+    print("Training models finished..")
+except BaseException:
+    print("Could not train models..skipping..")
+traceback.print_exc()
